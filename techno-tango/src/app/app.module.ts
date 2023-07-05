de06413dec32def3647b0e4b41b6692b9bde264f
Leon Smit <108675861+leon-smit-tpc@users.noreply.github.com>
2023-07-05 14:46:33 +0200

    add linting to frontend (#7)
    * add linting to frontend
    
    * change to default angular linting
    
    * add rules for linting
    
    * update plugins eslint
    
    * update linting for unused imports

--- techno-tango/src/app/app.module.ts
+++ techno-tango/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
 import { TodoListModule } from './todo-list/todo-list.module';
 
 @NgModule({
