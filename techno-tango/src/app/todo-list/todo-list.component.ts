b45fe386f9409d8629637a2df250bab7847b069a
Leon Smit <leon.smit@topicus.nl>
2023-07-05 14:04:16 +0200

    updated test for todo-list component

--- techno-tango/src/app/todo-list/todo-list.component.ts
+++ techno-tango/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TodoItem } from './../models/todo';
 import { TodoListService } from '../services/todo-list/todo-list.service';
@@ -8,17 +8,11 @@ import { TodoListService } from '../services/todo-list/todo-list.service';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent {
 
   todoList: Observable<TodoItem[]>;
   
   constructor(private todoListService: TodoListService) {
     this.todoList = this.todoListService.getTodoList();
-  }
-
-  ngOnInit(): void {
-    this.todoList.subscribe(todos => {
-      todos.forEach(todoItem => console.log(todoItem))
-    })
   }
 }
